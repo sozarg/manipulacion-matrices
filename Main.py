e3a2acc260f51a79cdedd49fcb8c372418dc0d06
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-11-13 16:01:48 -0300

    carga secuencial

--- Main.py
+++ Main.py
@@ -11,3 +11,11 @@ def mostrar_matriz(matriz:list) -> None:
             print(matriz[i][j], end= "\t")
         print()
 
+def carga_secuencial(matriz:list) -> list:
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            matriz[i][j] += int(input(f"Ingrese el elemento para la fila ({i}) | colummna ({j}): "))
+
+matriz = incializar_matriz(3,3,0)
+carga_secuencial(matriz)
+mostrar_matriz(matriz)
