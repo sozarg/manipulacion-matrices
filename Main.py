4f1aaede890bedf9471538254f5e2b7153ca3bbd
Mateo Terrile <131791764+sozarg@users.noreply.github.com>
2024-11-13 15:53:18 -0300

    inicializar matriz

--- Main.py
+++ Main.py
@@ -1,1 +1,7 @@
-print("Testedsd")
+def incializar_matriz(cantidad_filas:int, cantidad_columnas:int, valor_inicial:any) -> list:
+    matriz = []
+    for i in range(cantidad_filas):
+        filas = [valor_inicial] * cantidad_columnas
+        matriz += [filas]
+    return matriz
+
